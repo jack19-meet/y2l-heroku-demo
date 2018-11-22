2864dfed788b7a4428512ff016f0afb072fc1cca
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:09:03 +0200

    hi

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+    return "something creative"
 
 if __name__ == '__main__':
     app.run(debug=True)
